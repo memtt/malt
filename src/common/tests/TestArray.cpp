8e7feccf5ce1fa7c3a2e22f818623816be1b1ed0
Sebastien Valat <sebastien.valat@exascale-computing.eu>
2014-04-18 09:54:20 +0200

    * Start to move tests into subdirectories

--- src/common/tests/TestArray.cpp
+++ src/common/tests/TestArray.cpp
@@ -8,7 +8,7 @@
 
 /********************  HEADERS  *********************/
 #include <gtest/gtest.h>
-#include <common/Array.hpp>
+#include "Array.hpp"
 
 /***************** USING NAMESPACE ******************/
 using namespace MATT;
@@ -16,6 +16,7 @@ using namespace MATT;
 /*******************  FUNCTION  *********************/
 TEST(Array,constructor)
 {
+	//check allocation of default size
 	Array<int> array(10,64,true);
 	EXPECT_EQ(0,array.getSize());
 	EXPECT_EQ(10,array.getBufferSize());
@@ -24,14 +25,18 @@ TEST(Array,constructor)
 /*******************  FUNCTION  *********************/
 TEST(Array,push_back)
 {
+	//prepare array
 	Array<int> array(10,64,true);
 	
+	//fill
 	for (int i = 0 ; i < 20 ; i++)
 		array.push_back(i);
 	
+	//check current size
 	EXPECT_EQ(20,array.getSize());
 	EXPECT_EQ(20,array.getBufferSize());
 	
+	//check content and operator []
 	for (int i = 0 ; i < 20 ; i++)
 		ASSERT_EQ(array[i],i);
 }
