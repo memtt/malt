949cede83ba261ddb3b8a4967551046a10d0d183
Sebastien Valat <sebastien.valat@gmail.com>
2016-05-28 16:21:05 +0200

    * Start to add new webview based on yoman generator

---
+++ src/webview/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mattWebviewApp
+ * @description
+ * # mattWebviewApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mattWebviewApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ src/webview/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mattWebviewApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mattWebviewApp
+ */
+angular.module('mattWebviewApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
