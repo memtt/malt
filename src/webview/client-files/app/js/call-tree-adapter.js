2b4b391080a0a7ceeea49d8db251391eba52f0e5
Mehdi Raza Jaffery <syed.mehdi.raza.jaffery@cern.ch>
2016-07-19 12:09:15 +0200

    * Refactored code spinning dot code generation, call-tree data adapter and id cache into their own classes. * Added comments to all new classes. * Removed left-over reference to vis.js from index.html.

---
+++ src/webview/client-files/app/js/call-tree-adapter.js
@@ -0,0 +1,187 @@
+/**
+ * An adapter class that encapsulates a stack-tree and exposes
+ * it as nodes and edges, suitable for making a calltree graph.
+ * 
+ * @param {object} stacktree Stack tree object
+ */
+function CallTreeAdapter(stacktree)
+{
+	/**
+	 * Reduce child and parent statistics
+	 * @param  {object} node Child node
+	 * @param  {object} info Parent node info
+	 */
+	function reduceStat(node,info)
+	{
+		if(node.info == undefined)
+			node.info = jQuery.extend(true, {}, info);
+		else
+			maltHelper.mergeStackInfoDatas(node.info,info);
+	}
+
+	/**
+	 * Builds a hierarchical call tree from a stack tree
+	 * @param  {object} data Stack tree data
+	 * @return {object}      Call tree
+	 */
+	function buildCallTree(data)
+	{
+		var tree = {childs:{},id:null};
+		var id = 0;
+		data.forEach(function(call) {
+			var cur = tree;
+			reduceStat(cur,call.info);
+			call.stack.reverse().forEach(function(loc) {
+				if (cur.childs[loc.function] == undefined)
+					cur.childs[loc.function] = {childs:{},id:id++,location:loc};
+				cur = cur.childs[loc.function];
+				reduceStat(cur,call.info);
+			});
+		});
+		
+		return tree;
+	}
+
+	/**
+	 * Creates identifier from a location object for a function
+	 * @param  {object} location Location object for a function
+	 * @return {string}          Identifier
+	 */
+	function getIdentifier(location) 
+	{
+		if(!location)
+			return null;
+
+		return location.binary + '\\' 
+				+ location.file + '\\'
+				+ location.function + '\\'
+				+ location.line;
+	}
+
+	/**
+	 * Traverse call tree and generate nodes and edges
+	 * @param  {object} tree      Hierarchical Call-tree object
+	 * @param  {int} level        Current depth
+	 * @param  {array} nodes      Nodes array
+	 * @param  {array} vertices   Edges array
+	 * @param  {SimpleIdCache} nodeCache Cache to track node ids
+	 * @param  {SimpleIdCache} vertCache Cache to track edge
+	 * @return {int}              Id for current tree
+	 */
+	function generateNodesAndVertices(tree, level, nodes, vertices, nodeCache, vertCache)
+	{
+		var identifier = null;
+		var currentId = null;
+
+
+		if(tree.location) {
+			// Remove libmalt.so related functions captured in call tree
+			if(tree.location.binary.endsWith("libmalt.so"))
+				return null;
+
+			// Remove useless nodes
+			if(tree.info.alloc.count == 0)
+				return null;
+
+			identifier = getIdentifier(tree.location);
+
+			// Add node to node-list if this is a new node
+			if(!nodeCache.exists(identifier)) {
+				currentId = nodeCache.put(identifier);
+				
+				nodes.push({
+					id: currentId,
+					label: tree.location.function,
+					level: level,
+					score: tree.info.alloc.count
+				});
+			} else {
+				currentId = nodeCache.get(identifier);
+			}
+		} 
+
+		// Create edge from this node to all its children
+		for (var i in tree.childs) {
+			if(identifier !=  null) {
+				var childId = generateNodesAndVertices(tree.childs[i], level + 1, nodes, vertices, nodeCache, vertCache);
+				if(childId != null && !vertCache.exists(currentId + "," + childId)) {
+					vertCache.put(currentId + "," + childId);
+					vertices.push({
+						from: currentId,
+						to: childId
+					});					
+				}
+			} else {
+				generateNodesAndVertices(tree.childs[i], level + 1, nodes, vertices, nodeCache, vertCache);
+			}
+		}
+
+		return currentId;
+	}
+
+	/**
+	 * Convert a call tree to a object containing nodes and vertices
+	 * @param  {object} tree Call-tree
+	 * @return {object}      Contains nodes and edges
+	 */
+	function generateTreeDataSet(tree) 
+	{
+		var nodes = [], vertices= [];
+		generateNodesAndVertices(tree, 0, nodes, vertices, new SimpleIdCache(), new SimpleIdCache());
+
+		return {
+			nodes: nodes, 
+			edges: vertices
+		};
+	}
+
+	/**
+	 * Calculates color codes for nodes based on a metric
+	 *
+	 * Color is added as the 'color' property on each node
+	 * 
+	 * @param {dataset} dataset Call-Tree dataset
+	 */
+	function addColorCodes(dataset) {
+		var nodes = dataset.nodes;
+		var max = -1;
+
+		// find max
+		for (var i = 0; i < nodes.length; i++) {
+			if(nodes[i].score > max) {
+				max = nodes[i].score;
+			}
+		}
+
+		// generate a mapping function from [0-max] onto [#397EBA,#FF9595]
+		var colorScale = d3.scale.linear()
+			.range(["#397EBA","#ab4141"])
+			.domain([0,max]);
+
+		// assign colors
+		for (var i = 0; i < nodes.length; i++) {
+			nodes[i].color = colorScale(nodes[i].score);
+		}
+	}
+
+	var tree = generateTreeDataSet(buildCallTree(stacktree));
+	addColorCodes(tree);
+	
+	/**
+	 * Get edges for the Call-tree
+	 * @return {array} Array of edges {from, to}
+	 */
+	this.getEdges = function() {
+		return tree.edges;
+	}
+
+	/**
+	 * Get nodes for the call-tree
+	 * @return {array} Array of nodes {id, label, level, score}
+	 */
+	this.getNodes = function() {
+		return tree.nodes;
+	}
+
+	return this;
+}
--- src/webview/client-files/app/js/page-calltree.js
+++ src/webview/client-files/app/js/page-calltree.js
@@ -1,183 +1,49 @@
+/**
+ * Provide a call-tree graph which shows all the function calls,
+ * the allocations alongwith UI for navigation.
+ */
 function MaltPageCallTree() 
 {
-	function SimpleCache() {
-		this.cache = [];
-		this.id = 1;
-
-		this.exists = function(value) {
-			if(this.cache[value] === undefined)
-				return false;
-			return true;
-		}
-
-		this.put = function(value) {
-			if(this.exists(value))
-				return this.cache[value];
-			this.cache[value] = this.id++;
-			return this.cache[value];
-		}
-
-		this.get = function(value) {
-			if(!this.exists(value))
-				return false;
-			return this.cache[value];
-		}
-
-		return this;
-	}
-
-	function reduceStat(node,info)
+	function getDotCodeForTree(tree)
 	{
-		if(node.info == undefined)
-			node.info = jQuery.extend(true, {}, info);
-		else
-			maltHelper.mergeStackInfoDatas(node.info,info);
-	}
+		var nodes = tree.getNodes(),
+			vertices = tree.getEdges();
 
-	function buildCallTree(data)
-	{
-		var tree = {childs:{},id:null};
-		var id = 0;
-		data.forEach(function(call) {
-			var cur = tree;
-			reduceStat(cur,call.info);
-			call.stack.reverse().forEach(function(loc) {
-				if (cur.childs[loc.function] == undefined)
-					cur.childs[loc.function] = {childs:{},id:id++,location:loc};
-				cur = cur.childs[loc.function];
-				reduceStat(cur,call.info);
-			});
-		});
-		
-		return tree;
-	}
-
-	function getIdentifier(location) 
-	{
-		if(!location)
-			return null;
-
-		return location.binary + '\\' 
-				+ location.file + '\\'
-				+ location.function + '\\'
-				+ location.line;
-	}
-
-	function generateNodesAndVertices(tree, level, nodes, vertices, nodeCache, vertCache)
-	{
-		var identifier = null;
-		var currentId = null;
-
-
-		if(tree.location) {
-			// Remove libmalt.so related functions captured in call tree
-			if(tree.location.binary.endsWith("libmalt.so"))
-				return null;
-
-			// Remove useless nodes
-			if(tree.info.alloc.count == 0)
-				return null;
-
-			identifier = getIdentifier(tree.location);
-
-			// Add node to node-list if this is a new node
-			if(!nodeCache.exists(identifier)) {
-				currentId = nodeCache.put(identifier);
-				
-				nodes.push({
-					id: currentId,
-					label: tree.location.function,
-					level: level,
-					score: tree.info.alloc.count
+		return new DotCode()
+			.digraph('G', function(d) {
+				d.node({
+					shape: 'box',
+					fontname: 'Courier New',
+					fontsize: '12'
+				}).edge({
+					color: '#868a8f',
+					penwidth: 1.5
 				});
-			} else {
-				currentId = nodeCache.get(identifier);
-			}
-		} 
 
-		// Create edge from this node to all its children
-		for (var i in tree.childs) {
-			if(identifier !=  null) {
-				var childId = generateNodesAndVertices(tree.childs[i], level + 1, nodes, vertices, nodeCache, vertCache);
-				if(childId != null && !vertCache.exists(currentId + "," + childId)) {
-					vertCache.put(currentId + "," + childId);
-					vertices.push({
-						from: currentId,
-						to: childId
-					});					
+				for (var i = 0; i < nodes.length; i++) {
+					d.node('node' + nodes[i].id, {
+						shape: 'record', 
+						label: nodes[i].label.trim() + ' | ' + nodes[i].score, 
+						style: 'filled',
+						color: 'white',
+						fontcolor: 'white',
+						fillcolor: nodes[i].color
+					});
 				}
-			} else {
-				generateNodesAndVertices(tree.childs[i], level + 1, nodes, vertices, nodeCache, vertCache);
-			}
-		}
 
-		return currentId;
+				for (var i = 0; i < vertices.length; i++) {
+					d.edge('node' + vertices[i].from, 'node' + vertices[i].to);
+				}
+			})
+			.toCode();
 	}
 
-	function generateTreeDataSet(tree) 
-	{
-		var nodes = [], vertices= [];
-		generateNodesAndVertices(tree, 0, nodes, vertices, new SimpleCache(), new SimpleCache());
-
-		return {
-			nodes: nodes, 
-			edges: vertices
-		};
-	}
-
-	function generateDotCode(treeDataSet)
-	{
-		var nodes = treeDataSet.nodes,
-			vertices = treeDataSet.edges;
-		var nodesArr = [], verticeArr = [];
-		for (var i = 0; i < nodes.length; i++) {
-			nodesArr.push("node" + nodes[i].id + "[shape=record,label=\"" + nodes[i].label.trim() + " | " + nodes[i].score +"\",style=filled,color=white,fontcolor=white,fillcolor=\"" + nodes[i].color +"\"]");
-		}
-		for (var i = 0; i < vertices.length; i++) {
-			verticeArr.push("node" + vertices[i].from + " -> node" + vertices[i].to);
-		}
-		return "digraph G {\nnode[shape=\"box\",fontname=\"Courier New\", fontsize=\"12\"];\n" + nodesArr.join(";\n") + ";\n" + verticeArr.join(";\n") + ";\}";
-	}
-
-	function addColorCodes(dataset) {
-		var nodes = dataset.nodes;
-		var max = -1;
-
-		// find max
-		for (var i = 0; i < nodes.length; i++) {
-			if(nodes[i].score > max) {
-				max = nodes[i].score;
-			}
-		}
-
-		// generate a mapping function from [0-max] onto [#397EBA,#FF9595]
-		var colorScale = d3.scale.linear()
-			.range(["#397EBA","#ab4141"])
-			.domain([0,max]);
-
-		// assign colors
-		for (var i = 0; i < nodes.length; i++) {
-			nodes[i].color = colorScale(nodes[i].score);
-		}
-	}
 
 	maltCtrl.controller('malt.page.calltree.ctrl',['$scope','$routeParams','$http', function($scope,$routeParams,$http) {
 		maltDataSource.getCallStackDataFunc("_start",function(data) {
-			var tree = buildCallTree(data);
-			console.log("tree", tree);
-			var dataset = generateTreeDataSet(tree);
-			console.log("nodes", dataset.nodes);
-			console.log("vertices", dataset.edges);
+			var tree = new CallTreeAdapter(data);
+			var src = getDotCodeForTree(tree);
 
-			var container = document.getElementById('mynetwork');
-
-			for (var i = 0; i < dataset.nodes.length; i++) {
-				dataset.nodes[i].label = dataset.nodes[i].label + '\n';
-			}
-
-			addColorCodes(dataset);
-			var src = generateDotCode(dataset);
-			console.log(src);
 			var result = Viz(src, { format:"svg", engine:"dot" });
 			var parser = new DOMParser();
 			var svg = parser.parseFromString(result, "image/svg+xml");
