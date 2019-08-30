128add7fc66a2dafefaf8b33c9d83cc8f4c02287
Sebastien Valat <sebastien.balat.dev@orange.fr>
2019-08-30 12:18:38 +0200

    [scripts] Now should update all version flags

--- dev/update-version.sh
+++ dev/update-version.sh
@@ -54,65 +54,98 @@ echo "New version is ${newversion}"
 echo "Date is ${newdate}"
 
 ######################################################
-find ./ | while read file
-do
-	#exclude directories
-	if [ -d "$file" ]; then continue; fi
-
-	#exclude some paths
-	case "$file" in
-		*node_modules*|*bower_components*|*.git*|*extern-deps*)
-			#echo "exclude $file"
-			continue
-			;;
-	esac
-
-	#exclude non MALT files
-	if [ -z "$(cat $file | grep MALT)" ] && [ -z "$(cat $file | grep malt)" ]
-	then
-		continue;
-	fi
-
-	#do replacement
-	sed -i -r -e "s#  DATE     : [0-9]{2}/[0-9]{4}#  ${newdate}#g" \
-	          -e "s#  $V  : .{16}#  ${newversion}#" \
-	          -e "s#  $V  : [0-9a-zA-Z.-]+\$#  $V  : ${version}#" \
-			  "${file}"
+echo
+echo
+for tmp in $(seq 1 5); do
+	echo "Will start in $((5 - $tmp)) seconds, CTRL+C to stop"
+	sleep 1
 done
+
+
+######################################################
+update_all_files()
+{
+	find ./ | while read file
+	do
+		#exclude directories
+		if [ -d "$file" ]; then continue; fi
+		if [ -L "$file" ]; then continue; fi
+
+		#exclude some paths
+		case "$file" in
+			*node_modules*|*bower_components*|*.git*|*extern-deps*)
+				#echo "exclude $file"
+				continue
+				;;
+		esac
+
+		#exclude non MALT files
+		if [ -z "$(cat $file | grep MALT)" ] && [ -z "$(cat $file | grep malt)" ]
+		then
+			continue;
+		fi
+
+		#do replacement
+		sed -i -r -e "s#  DATE     : [0-9]{2}/[0-9]{4}#  ${newdate}#g" \
+	        	  -e "s#  $V  : .{16}#  ${newversion}#" \
+		          -e "s#  $V  : [0-9a-zA-Z.-]+\$#  $V  : ${version}#" \
+				  "${file}"
+	done
+}
 
 ######################################################
 #update bower and package
-set -i -r -e "s/$OLD_VERSION_PROTECTED/${version}/g" src/integration/malt.sh.in
-sed -i -r -e "s/\"version\": \"[0-9A-Za-z.-]+\"/\"version\": \"${shortVersion}\"/g" src/webview/bower.json
-sed -i -r -e "s/\"version\": \"[0-9A-Za-z.-]+\"/\"version\": \"${shortVersion}\"/g" src/webview/package.json
-sed -i -r -e "s/^version=[0-9A-Za-z.-]+$/version=${version}/g" dev/gen-archive.sh
-sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" packaging/README.md
-sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" dev/packaging.sh
-sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" packaging/fedora/malt.spec
-sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" packaging/debian/changelog
-sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" src/manpages/*.ronn
-sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" packaging/gentoo/dev-util/malt/malt-*.ebuild
-sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" packaging/archlinux/PKGBUILD
-sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" Doxyfile
-sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" ./src/lib/CMakeLists.txt
+update_special_sed()
+{
+	sed -i -r -e "s/$OLD_VERSION_PROTECTED/${version}/g" src/integration/malt.sh.in
+	sed -i -r -e "s/\"version\": \"[0-9A-Za-z.-]+\"/\"version\": \"${shortVersion}\"/g" src/webview/bower.json
+	sed -i -r -e "s/\"version\": \"[0-9A-Za-z.-]+\"/\"version\": \"${shortVersion}\"/g" src/webview/package.json
+	sed -i -r -e "s/^version=[0-9A-Za-z.-]+$/version=${version}/g" dev/gen-archive.sh
+	sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" packaging/README.md
+	sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" dev/packaging.sh
+	sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" packaging/fedora/malt.spec
+	sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" packaging/debian/changelog
+	sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" src/manpages/*.ronn
+	sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" packaging/gentoo/dev-util/malt/malt-*.ebuild
+	sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" packaging/archlinux/PKGBUILD
+	sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" Doxyfile
+	sed -i -r -e "s/${OLD_SHORT_VERSION_PROTECTED}/${shortVersion}/g" src/lib/CMakeLists.txt
+	sed -i -r -e "s/${OLD_VERSION_PROTECTED}/${version}/g" src/lib/tests/TestValgrindOutput.cpp
+	sed -i -r -e "s/${OLD_VERSION_PROTECTED}/${version}/g" .vscode/snippets/cpp.json
+	sed -i -r -e "s/${OLD_VERSION_PROTECTED}/${version}/g" src/lib/tests/TestValgrindOutput.cpp
+	sed -i -r -e "s/${OLD_VERSION_PROTECTED}/${version}/g" CMakeLists.txt
+}
 
 ######################################################
-#update date in debian changelog
-sed -i -r -e "s/ [A-Z][a-z]{2}, [0-9]+ [A-Z][a-z]{2} [0-9]+ [0-9]{2}:[0-9]{2}:[0-9]{2} \+[0-9]{4}$/$(LANG=C date -R)/g" packaging/debian/changelog
+update_packaging()
+{
+	#update date in debian changelog
+	sed -i -r -e "s/ [A-Z][a-z]{2}, [0-9]+ [A-Z][a-z]{2} [0-9]+ [0-9]{2}:[0-9]{2}:[0-9]{2} \+[0-9]{4}$/$(LANG=C date -R)/g" packaging/debian/changelog
 
-######################################################
-#mv gentoo ebuild
-git mv packaging/gentoo/dev-util/malt/malt-${OLD_VERSION}.ebuild packaging/gentoo/dev-util/malt/malt-${version}.ebuild || exit 1
+	#mv gentoo ebuild
+	git mv packaging/gentoo/dev-util/malt/malt-${OLD_VERSION}.ebuild packaging/gentoo/dev-util/malt/malt-${version}.ebuild || exit 1
+}
 
 ######################################################
 #Check if manpages need to be recompiled
-make -C ./src/manpages || exit 1
+update_manpages()
+{
+	make -C ./src/manpages || exit 1
+}
 
 ######################################################
 #serach not updated
-echo "=================== Check not updated list ======================"
-grep -RHn "$(echo "${OLD_VERSION}" | sed -e 's/\./\\./g')" ./ | grep -v node_modules | grep -v extern-deps | grep -v "\.git" | grep -v bower_components | grep -v ChangeLog.md | grep -v build | grep "${OLD_VERSION}"
-if [ "$shortVerison" != "$OLD_SHORT_VERSION" ]; then
-	grep -RHn "${OLD_SHORT_VERSION_PROTECTED}" ./ | grep -v node_modules | grep -v extern-deps | grep -v "\.git" | grep -v bower_components | grep -v ChangeLog.md | grep -v build | grep "${OLD_SHORT_VERSION}"
-fi
-echo "================================================================="
+post_check()
+{
+	echo "=================== Check not updated list ======================"
+	grep -RHn "$(echo "${OLD_VERSION}" | sed -e 's/\./\\./g')" ./ | grep -v node_modules | grep -v extern-deps | grep -v "\.git" | grep -v bower_components | grep -v ChangeLog.md | grep -v build | grep "${OLD_VERSION}"
+	if [ "$shortVerison" != "$OLD_SHORT_VERSION" ]; then
+		grep -RHn "${OLD_SHORT_VERSION_PROTECTED}" ./ | grep -v node_modules | grep -v extern-deps | grep -v "\.git" | grep -v bower_components | grep -v ChangeLog.md | grep -v build | grep "${OLD_SHORT_VERSION}"
+	fi
+	echo "================================================================="
+}
+
+update_all_files
+update_special_sed
+update_manpages
+post_check
