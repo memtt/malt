b176e52e865700e140946569bf0877c0729ad1f2
Sebastien Valat <sebastien.valat.dev@orange.fr>
2022-01-27 12:26:50 +0100

    [test] Test portability compiler functions

---
+++ src/lib/portability/tests/TestCompiler.cpp
@@ -0,0 +1,75 @@
+/*****************************************************
+             PROJECT  : MALT
+             VERSION  : 1.2.2
+             DATE     : 10/2020
+             AUTHOR   : Valat Sébastien
+             LICENSE  : CeCILL-C
+*****************************************************/
+
+/********************  HEADERS  *********************/
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "Compiler.hpp"
+#include <common/Options.hpp>
+#include <common/SimpleAllocator.hpp>
+#include <core/SymbolSolver.hpp>
+
+/***************** USING NAMESPACE ******************/
+using namespace MALT;
+using namespace testing;
+
+
+/*******************  FUNCTION  *********************/
+extern "C" {
+	int notMangledCFunction(bool p1, int p2)
+	{
+		return p2;
+	}
+}
+
+/*******************  FUNCTION  *********************/
+std::string mangledCppFunction(bool p1, int p2)
+{
+	return "ok";
+}
+
+/*******************  FUNCTION  *********************/
+std::string getMangledName(void* addr)
+{
+	void * callsite = (void*)((size_t)addr + 1);
+	SymbolSolver solver;
+	solver.registerAddress(callsite);
+	solver.solveNames();
+	int funcId = solver.getCallSiteInfo(callsite)->function;
+	return solver.getString(funcId);
+}
+
+/*******************  FUNCTION  *********************/
+TEST(TestCompiler,demangleCppNames_mangled)
+{
+	std::string mangled = getMangledName((void*)mangledCppFunction);
+	std::string demangled = Compiler::demangleCppNames(mangled);
+	EXPECT_THAT(demangled, MatchesRegex("^mangledCppFunction(\\[.*\\])\\(bool, int\\)$")) << "mangled=" << mangled;
+}
+
+/*******************  FUNCTION  *********************/
+TEST(TestCompiler,demangleCppNames_not_mangled)
+{
+	std::string mangled = getMangledName((void*)notMangledCFunction);
+	std::string demangled = Compiler::demangleCppNames(mangled);
+	EXPECT_THAT(demangled, MatchesRegex("^notMangledCFunction()")) << "mangled=" << mangled;
+}
+
+/*******************  FUNCTION  *********************/
+int main(int argc, char ** argv)
+{
+	//init internal allocator
+	gblInternaAlloc = new SimpleAllocator(true);
+	
+	//ini gblOptions
+	initGlobalOptions();
+	
+	// This allows the user to override the flag on the command line.
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
