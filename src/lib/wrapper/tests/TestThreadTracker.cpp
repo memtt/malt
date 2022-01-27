c85e8e9d7daa2a186c11e026fcefcd502b315e40
Sebastien Valat <sebastien.valat.dev@orange.fr>
2022-01-28 00:13:25 +0100

    [test] Test ThreadWrapper

---
+++ src/lib/wrapper/tests/TestThreadTracker.cpp
@@ -0,0 +1,40 @@
+/*****************************************************
+             PROJECT  : MALT
+             VERSION  : 1.2.2
+             DATE     : 10/2020
+             AUTHOR   : Valat Sébastien
+             LICENSE  : CeCILL-C
+*****************************************************/
+
+/********************  HEADERS  *********************/
+#include <gtest/gtest.h>
+#include <thread>
+#include <ThreadTracker.hpp>
+
+/***************** USING NAMESPACE ******************/
+using namespace MALT;
+
+/*******************  FUNCTION  *********************/
+TEST(TestThreadTracker, no_threads)
+{
+	EXPECT_EQ(1, ThreadTracker::getThreadCount());
+	EXPECT_EQ(1, ThreadTracker::getMaxThreadCount());
+}
+
+/*******************  FUNCTION  *********************/
+TEST(TestThreadTracker, one_thread)
+{
+	std::thread threads[10];
+
+	for (int i = 0 ; i < 10 ; i++) {
+		threads[i] = std::thread([](){
+			usleep(1000);
+		});
+	}
+
+	for (int i = 0 ; i < 10 ; i++)
+		threads[i].join();
+
+	EXPECT_EQ(1, ThreadTracker::getThreadCount());
+	EXPECT_EQ(11, ThreadTracker::getMaxThreadCount());
+}
