0154504e9abcd6eb5bc110775d5b5a23bab61b6a
Sebastien Valat <sebastien.valat.dev@orange.fr>
2024-10-02 14:19:08 +0200

    [dev] Rewrite gen-archive.sh in python

---
+++ dev/gen-archive.py
@@ -0,0 +1,99 @@
+#!/bin/env python3
+############################################################
+#    PROJECT  : MALT (MALoc Tracker)
+#    VERSION  : 1.2.3
+#    DATE     : 10/2024
+#    LICENSE  : CeCILL-C
+#    FILE     : dev/gen-archive.sh
+#-----------------------------------------------------------
+#    AUTHOR   : Sébastien Valat - 2015 - 2024
+############################################################
+
+############################################################
+#This script aimed at generating archive to distribute
+#the project. Simply call it from root directory without
+#any agruments.
+
+############################################################
+import os
+import shutil
+import tempfile
+import subprocess
+from contextlib import contextmanager
+
+############################################################
+#extract version
+PACKAGE_NAME="malt"
+PACKAGE_VERSION="1.2.3"
+
+############################################################
+# Run command after logging it and intercept error
+def run_shell(cmd: str) -> None:
+    print(f"  + {cmd}")
+    subprocess.run(cmd, shell=True, check=True)
+
+############################################################
+@contextmanager
+def jump_in_dir(path: str):
+    '''
+    An helper semantic to jump temporarily in a directory. As wrapped with python 'with' semantic,
+    went back automatically on the previous one on error or success.
+    '''
+    # get old path
+    oldpwd = os.getcwd()
+
+    # chump in
+    os.chdir(path)
+
+    # handle return back afterward
+    try:
+        yield
+    finally:
+        os.chdir(oldpwd)
+
+############################################################
+@contextmanager
+def jump_in_tmpdir():
+    tmpdir = tempfile.mkdtemp()
+    try:
+        with jump_in_dir(tmpdir):
+            yield tmpdir
+    finally:
+        shutil.rmtree(tmpdir)
+
+############################################################
+# Generate MALT archive
+def malt_dev_gen_archive(name: str = PACKAGE_NAME, version: str = PACKAGE_VERSION) -> None:
+    # build default prefix
+    prefix=f"{name}-{version}"
+    
+    # check is not release
+    git_head_hash = subprocess.getoutput("git rev-parse --short HEAD")
+    git_version_hash = subprocess.getoutput(f"git rev-parse --short v{version}")
+    if git_head_hash != git_version_hash:
+        prefix=f"{name}-{git_head_hash}"
+
+    # log
+    print(f"Generate {prefix}.tar.bz2...")
+
+    # gen native archive
+    run_shell("git archive --format=tar --prefix=${prefix}/ HEAD | bzip2 > /tmp/${prefix}.tar.bz2")
+
+    # extract to fetch deps & regen
+    origdir = os.getcwd()
+    with jump_in_tmpdir() as dir:
+        run_shell(f"tar -xf {origdir}/{prefix}.tar.bz2")
+        with jump_in_dir(f"{prefix}/src/webview"):
+            print("  --------------------------------------------")
+            run_shell("./prepare.sh 2>&1 | sed -e 's/^/  /g'")
+            print("  --------------------------------------------")
+        run_shell(f"tar -cjf {prefix}.tar.bz2 {prefix}")
+        print(f"  + mv '{prefix}.tar.bz2' '{origdir}/{prefix}.tar.bz2'")
+        shutil.move(f"{prefix}.tar.bz2", f"{origdir}/{prefix}.tar.bz2")
+
+    # log
+    print("Done")
+
+############################################################
+if __name__ == "__main__":
+    malt_dev_gen_archive()
