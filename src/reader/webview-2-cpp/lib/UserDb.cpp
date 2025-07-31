6bd440303c023d2f3ce92f9f9cdd7c72a0919709
Sebastien Valat <sebastien.valat@inria.fr>
2025-07-31 18:30:44 +0200

    [webview] Fix issue with deprecated SSL function SHA256_Init

--- src/reader/webview-2-cpp/lib/UserDb.cpp
+++ src/reader/webview-2-cpp/lib/UserDb.cpp
@@ -117,14 +117,18 @@ std::string UserDb::calcDefaultDbPath(void)
 /**********************************************************/
 std::string UserDb::sha256sum(const std::string & value)
 {
+	//buffer
 	unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256_CTX sha256;
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256, value.c_str(), value.size());
-    SHA256_Final(hash, &sha256);
+    
+	//hash
+	SHA256((const unsigned char *)value.c_str(), value.size(), hash);
+
+	//convert to text
     std::stringstream ss;
     for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
         ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
+
+	//ok
     return ss.str();
 }
 
