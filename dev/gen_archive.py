ff75a0f45b2105a1b62033e66bf236e283361dae
Sebastien Valat <sebastien.valat@univ-grenoble-alpes.fr>
2026-08-18 19:30:15 +0200

    [version] Update version to 1.6.3

--- dev/gen_archive.py
+++ dev/gen_archive.py
@@ -25,7 +25,7 @@ from enum import Enum
 ############################################################
 #extract version
 PACKAGE_NAME="malt"
-PACKAGE_VERSION="1.6.2"
+PACKAGE_VERSION="1.6.3"
 
 ############################################################
 class HashMode(Enum):
